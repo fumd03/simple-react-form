c47c9bcffd124ce0c297ab8dd4e68b3a38e99248
fuad <fumd03@gmail.com>
2025-04-05 10:31:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./react_form1"; 
 
-import App from "./react_form1";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
